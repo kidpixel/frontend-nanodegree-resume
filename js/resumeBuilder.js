89d65a20dbc5991a9de96a281609877f374436fb
Carlo Jonson <cj811k@att.com>
2014-11-24 01:45:13 -0500

    finalize correct array code for lesson module

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,16 +14,11 @@
 //$("#main").append(funThoughts);
 
 
-
-
 var name = "Carlo M. Jonson"
 var role = "Web Developer"
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);	
-
-console.log(formattedName);
-console.log(formattedRole);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);	
@@ -38,8 +33,46 @@ $("#main").append(udacious);
 */
 
 // My first way of approaching the last element... this modifieds the specific last element "2", not necessarily the "last"
+/*
 var sampleArray = [0,0,7];
 sampleArray[2] = sampleArray[2] + 1;
 var newArray = sampleArray;
 console.log(newArray[2]);
+*/
 
+// My second attempt per the push/pop methods
+/*
+var sampleArray = [0,0,7];
+var popped = sampleArray.pop()+1;
+sampleArray.push(popped);   // can't store this in newArray because it only returns the length of the array and not the resulting array itself.
+var newArray = sampleArray;
+console.log(popped);
+console.log(newArray[2]);  // correctly shows 8 now.
+console.log(sampleArray);  // correctly shows [0,0,8] now.
+*/
+
+//-----------------------------------------------Final code that passed
+var sampleArray = [0,0,7];
+
+var incrementLastArrayElement = function(_array) {
+    var newArray = [];
+    // Your code should make newArray equal to an array that has the same
+    // values as _array, but the last number has increased by one.
+    
+    // For example:
+    // _array = [1, 2, 3];
+    // turn into:
+    // newArray = [1, 2, 4];
+    
+    // Your code goes in here!
+    var popped = _array.pop()+1;
+    _array.push(popped);   // can't store this in newArray because it only returns the length of the array and not the resulting array itself.
+    newArray = _array;
+    
+    // Don't delete this line!
+    return newArray;
+};
+
+// Did your code work? The line below will tell you!
+console.log(incrementLastArrayElement(sampleArray));
+//----------------------------------------------------------------------
